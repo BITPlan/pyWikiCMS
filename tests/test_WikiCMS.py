a07e95d65b7262fe137f53ed10c61af5e43ad73f
Wolfgang Fahl <wf@bitplan.com>
2020-07-28 21:01:14 +0200

    changes tests to open research

--- tests/test_WikiCMS.py
+++ tests/test_WikiCMS.py
@@ -4,11 +4,24 @@ Created on 27.07.2020
 @author: wf
 '''
 import unittest
-
+import os
+from wikibot.wikibot import WikiBot
 
 class TestWikiCMS(unittest.TestCase):
 
-
+    @staticmethod
+    def getSMW_Wiki(wikiId="cr"):
+        iniFile=WikiBot.iniFilePath(wikiId)
+        if not os.path.isfile(iniFile):
+            if wikiId=="smw":
+                WikiBot.writeIni(wikiId,"Semantic MediaWiki","https://www.semantic-mediawiki.org","/w","MediaWiki 1.31.7")
+            if wikiId=="or":
+                WikiBot.writeIni(wikiId,"OpenResearch.org","https://www.openresearch.org","/mediawiki/","MediaWiki 1.31.1")    
+            if wikiId=="cr":
+                WikiBot.writeIni(wikiId,"Conference Lookup","http://cr.bitplan.com","/","MediaWiki 1.33.2")    
+        wikibot=WikiBot.ofWikiId(wikiId)
+        return  wikibot
+    
     def setUp(self):
         pass
 
@@ -18,7 +31,12 @@ class TestWikiCMS(unittest.TestCase):
 
 
     def testWikiCMS(self):
-        
+        ''' test CMS access '''
+        wikibot=TestWikiCMS.getSMW_Wiki("or")
+        pageTitle="Main Page"
+        page=wikibot.getPage(pageTitle)
+        # print(page.text)
+        self.assertTrue("OpenResearch" in page.text)
         pass
 
 
