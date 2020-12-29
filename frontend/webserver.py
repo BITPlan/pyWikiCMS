54666b06c3ea409e1d4852acd8e0ccfcbc3a3495
Wolfgang Fahl <wf@bitplan.com>
2020-12-29 09:37:20 +0100

    adds flush to debug output

--- frontend/webserver.py
+++ frontend/webserver.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     args=parser.parse_args()
     if args.debugServer:
         import pydevd
-        print (args.debugPathMapping)
+        print (args.debugPathMapping,flush=True)
         if args.debugPathMapping:
             if len(args.debugPathMapping)==2:
                 remotePath=args.debugPathMapping[0]
