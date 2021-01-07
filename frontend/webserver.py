1fd0e8934864e63431a4711296cee54e9e06b449
Wolfgang Fahl <wf@bitplan.com>
2021-01-07 12:30:08 +0100

    fixes decorators

--- frontend/webserver.py
+++ frontend/webserver.py
@@ -8,45 +8,9 @@ from pydevd_file_utils import setup_client_server_paths
 import argparse
 import sys
 from frontend.app import AppWrap
-     
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Wiki Content Management webservice")
-    parser.add_argument('--baseUrl',default='',help='base url to use for links to this site')
-    parser.add_argument('--debug',
-                                 action='store_true',
-                                 help="run in debug mode")
-    parser.add_argument('--debugServer',
-                                 help="remote debug Server")
-    parser.add_argument('--debugPort',type=int,
-                                 help="remote debug Port",default=5678)
-    parser.add_argument('--debugPathMapping',nargs='+',help="remote debug Server path mapping - needs two arguments 1st: remotePath 2nd: local Path")
-    parser.add_argument('--sites',nargs='+',required=False,help="the sites to enable")
-    args=parser.parse_args()
-    if args.debugServer:
-        import pydevd
-        print (args.debugPathMapping,flush=True)
-        if args.debugPathMapping:
-            if len(args.debugPathMapping)==2:
-                remotePath=args.debugPathMapping[0] # path on the remote debugger side
-                localPath=args.debugPathMapping[1]  # path on the local machine where the code runs
-                MY_PATHS_FROM_ECLIPSE_TO_PYTHON = [
-                    (remotePath, localPath),
-                ]
-                setup_client_server_paths(MY_PATHS_FROM_ECLIPSE_TO_PYTHON)
-                #os.environ["PATHS_FROM_ECLIPSE_TO_PYTHON"]='[["%s", "%s"]]' % (remotePath,localPath)
-                #print("trying to debug with PATHS_FROM_ECLIPSE_TO_PYTHON=%s" % os.environ["PATHS_FROM_ECLIPSE_TO_PYTHON"]);
-     
-        pydevd.settrace(args.debugServer, port=args.debugPort,stdoutToServer=True, stderrToServer=True)
-        print("command line args are: %s" % str(sys.argv))
-    appWrap=AppWrap()
-    app=appWrap.app      
-    appWrap.debug=args.debug
-    appWrap.baseUrl=args.baseUrl
-    appWrap.enableSites(args.sites)
-    appWrap.run()
     
+appWrap=AppWrap()
+app=appWrap.app 
 @app.route('/')
 def admin():
     return appWrap.family()
@@ -85,4 +49,40 @@ def wrap(path):
     '''
     site,path=AppWrap.splitPath(path)
     return appWrap.wrap(site,path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Wiki Content Management webservice")
+    parser.add_argument('--baseUrl',default='',help='base url to use for links to this site')
+    parser.add_argument('--debug',
+                                 action='store_true',
+                                 help="run in debug mode")
+    parser.add_argument('--debugServer',
+                                 help="remote debug Server")
+    parser.add_argument('--debugPort',type=int,
+                                 help="remote debug Port",default=5678)
+    parser.add_argument('--debugPathMapping',nargs='+',help="remote debug Server path mapping - needs two arguments 1st: remotePath 2nd: local Path")
+    parser.add_argument('--sites',nargs='+',required=False,help="the sites to enable")
+    args=parser.parse_args()
+    if args.debugServer:
+        import pydevd
+        print (args.debugPathMapping,flush=True)
+        if args.debugPathMapping:
+            if len(args.debugPathMapping)==2:
+                remotePath=args.debugPathMapping[0] # path on the remote debugger side
+                localPath=args.debugPathMapping[1]  # path on the local machine where the code runs
+                MY_PATHS_FROM_ECLIPSE_TO_PYTHON = [
+                    (remotePath, localPath),
+                ]
+                setup_client_server_paths(MY_PATHS_FROM_ECLIPSE_TO_PYTHON)
+                #os.environ["PATHS_FROM_ECLIPSE_TO_PYTHON"]='[["%s", "%s"]]' % (remotePath,localPath)
+                #print("trying to debug with PATHS_FROM_ECLIPSE_TO_PYTHON=%s" % os.environ["PATHS_FROM_ECLIPSE_TO_PYTHON"]);
+     
+        pydevd.settrace(args.debugServer, port=args.debugPort,stdoutToServer=True, stderrToServer=True)
+        print("command line args are: %s" % str(sys.argv))     
+    appWrap.debug=args.debug
+    appWrap.baseUrl=args.baseUrl
+    appWrap.enableSites(args.sites)
+    appWrap.run()
+    
+
     
