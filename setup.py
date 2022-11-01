371ad8df116776914721dc8b2e53ceb050d05b0b
Wolfgang Fahl <wf@bitplan.com>
2022-11-01 08:16:05 +0100

    upgrades

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except:
     long_description = open('../../README.md').read()
 
 setup(name='pyWikiCMS',
-      version='0.1.0',
+      version='0.2.0',
       description='python implementation of a Mediawiki based Content Management System',
       long_description=long_description,
       long_description_content_type='text/markdown',
