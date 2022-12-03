f093857a830eb8a5671336c9ddf6789d9b1a6835
Wolfgang Fahl <wf@bitplan.com>
2022-12-03 16:03:45 +0100

    fixes #13

---
+++ frontend/cmsapp.py
@@ -0,0 +1,124 @@
+'''
+Created on 2022-11-03
+
+@author: wf
+'''
+import os
+from jpcore.compat import Compatibility;Compatibility(0,11,3)
+from jpcore.justpy_config import JpConfig
+script_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = script_dir+"/resources/static"
+JpConfig.set("STATIC_DIRECTORY",static_dir)
+# shut up justpy
+JpConfig.set("VERBOSE","False")
+JpConfig.setup()
+from jpwidgets.bt5widgets import App,About,ProgressBar,Switch
+from wikibot.wikiuser import WikiUser
+
+class CmsApp(App):
+    """
+    Content Management System application
+    """
+    
+    def __init__(self,version,title:str,args:None):
+        '''
+        Constructor
+        
+        Args:
+            version(Version): the version info for the app
+        '''
+        import justpy as jp
+        self.jp=jp
+        App.__init__(self, version=version,title=title)
+        self.args=args
+        self.addMenuLink(text='Home',icon='home', href="/")
+        self.addMenuLink(text='github',icon='github', href=version.cm_url)
+        self.addMenuLink(text='Chat',icon='chat',href=version.chat_url)
+        self.addMenuLink(text='Documentation',icon='file-document',href=version.doc_url)
+        self.addMenuLink(text='Settings',icon='cog',href="/settings")
+        self.addMenuLink(text='About',icon='information',href="/about")
+        
+        jp.Route('/settings',self.settings)
+        jp.Route('/about',self.about)
+        # wiki users
+        self.wikiUsers=WikiUser.getWikiUsers()
+        
+    async def onPageReady(self,_msg):
+        """
+        react on page Ready
+        """
+        try:
+            pass
+            #if len(self.gridRows.components)==0:
+            #    await self.showGenerateGrid()
+        except BaseException as ex:
+            self.handleException(ex)
+        
+    def setupRowsAndCols(self):
+        """
+        setup the general layout
+        """
+        head_html="""<link rel="stylesheet" href="/static/css/md_style_indigo.css">"""
+        self.wp=self.getWp(head_html)
+        self.button_classes = """btn btn-primary"""
+        # rows
+        self.rowA=self.jp.Div(classes="row",a=self.contentbox)
+        self.rowB=self.jp.Div(classes="row",a=self.contentbox)
+        self.rowC=self.jp.Div(classes="row",a=self.contentbox)
+        self.rowD=self.jp.Div(classes="row",a=self.contentbox)
+        self.rowE=self.jp.Div(classes="row",a=self.contentbox)
+        # columns
+        self.colA1=self.jp.Div(classes="col-12",a=self.rowA)
+        self.colB1=self.jp.Div(classes="col-3",a=self.rowB)
+        self.colB2=self.jp.Div(classes="col-2",a=self.rowB)
+        self.colC1=self.jp.Div(classes="col-3",a=self.rowC)
+        self.colC2=self.jp.Div(classes="col-2",a=self.rowC)
+        self.colD1=self.jp.Div(classes="col-12",a=self.rowD)
+        self.colE1=self.jp.Div(classes="col-12",a=self.rowE)
+        # standard elements
+        self.errors=self.jp.Div(a=self.colA1,style='color:red')
+        self.messages=self.jp.Div(a=self.colE1,style='color:black')  
+        self.progressBar = ProgressBar(a=self.rowC)
+
+    
+    async def settings(self)->"jp.WebPage":
+        '''
+        settings
+        
+        Returns:
+            jp.WebPage: a justpy webpage renderer
+        '''
+        self.setupRowsAndCols()
+        return self.wp
+    
+    async def about(self)->"jp.WebPage":
+        '''
+        show about dialog
+        
+        Returns:
+            jp.WebPage: a justpy webpage renderer
+        '''
+        self.setupRowsAndCols()
+        self.aboutDiv=About(a=self.colB1,version=self.version)
+
+        return self.wp
+    
+    async def content(self)->"jp.WebPage":
+        '''
+        provide the main content page
+        
+        Returns:
+            jp.WebPage: a justpy webpage renderer
+        '''
+        self.setupRowsAndCols()
+        #await self.add_or_update_context_select()
+        self.wp.on("page_ready", self.onPageReady)
+        return self.wp
+    
+    def start(self,host,port,debug):
+        """
+        start the server
+        """
+        self.debug=debug
+        import justpy as jp
+        jp.justpy(self.content,host=host,port=port)
---
+++ frontend/cmsmain.py
@@ -0,0 +1,102 @@
+'''
+Created on 2022-12-03
+
+@author: wf
+'''
+'''
+Created on 2022-11-24
+
+@author: wf
+'''
+
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+from frontend.version import Version
+from frontend.cmsapp import CmsApp
+import os
+import sys
+import traceback
+import webbrowser
+# import after app!
+from jpcore.justpy_app import JustpyServer
+
+class CmsMain():
+    """
+    ContentManagement System Main Program
+    """
+
+__version__ = Version.version
+__date__ = Version.date
+__updated__ = Version.updated
+
+
+def main(argv=None): # IGNORE:C0111
+    '''main program.'''
+
+    if argv is None:
+        argv=sys.argv[1:]
+    
+    program_name = os.path.basename(__file__)
+    program_shortdesc = Version.description
+    
+    program_version =f"v{__version__}" 
+    program_build_date = str(__updated__)
+    program_version_message = f'{program_name} ({program_version},{program_build_date})'
+
+    user_name="Wolfgang Fahl"
+    program_license = '''%s
+
+  Created by %s on %s.
+  Copyright 2022 Wolfgang Fahl. All rights reserved.
+
+  Licensed under the Apache License 2.0
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Distributed on an "AS IS" basis without warranties
+  or conditions of any kind, either express or implied.
+
+USAGE
+''' % (program_shortdesc, user_name,str(__date__))
+    try:
+        # Setup argument parser
+        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+        parser.add_argument("--about",help="show about info [default: %(default)s]",action="store_true")
+        parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="show debug info [default: %(default)s]")
+        parser.add_argument('--host',default=JustpyServer.getDefaultHost(),help="the host to serve / listen from [default: %(default)s]")
+        parser.add_argument('--port',type=int,default=8252,help="the port to serve from [default: %(default)s]")
+        parser.add_argument("--serve",help="start webserver",action="store_true")
+        parser.add_argument('-V', '--version', action='version', version=program_version_message)
+        args = parser.parse_args(argv)
+        if len(argv) < 1:
+            parser.print_usage()
+            sys.exit(1)
+        #ypgen=YPGen(args)
+        if args.about:
+            print(program_version_message)
+            print(f"see {Version.doc_url}")
+            webbrowser.open(Version.doc_url)
+        elif args.serve:
+            app=CmsApp(version=Version, title="pyWikiCMS",args=args)
+            url=f"http://{args.host}:{args.port}"
+            webbrowser.open(url)
+            app.start(host=args.host, port=args.port,debug=args.debug)
+            pass
+        pass
+    except KeyboardInterrupt:
+        ### handle keyboard interrupt ###
+        return 1
+    except Exception as e:
+        if DEBUG:
+            raise(e)
+        indent = len(program_name) * " "
+        sys.stderr.write(program_name + ": " + repr(e) + "\n")
+        sys.stderr.write(indent + "  for help use --help")
+        if args.debug:
+            print(traceback.format_exc())
+        return 2       
+        
+DEBUG = 1
+if __name__ == "__main__":
+    if DEBUG:
+        sys.argv.append("-d")
+    sys.exit(main())
